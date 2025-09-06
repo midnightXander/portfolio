89e89ae5f63fdb235287e0f8a05581a11ce9e1a7
Ngaikam Alex <alexngaikama913@gmail.com>
2025-09-06 09:48:34 +0100

    added type

--- src/components/mdx-content.tsx
+++ src/components/mdx-content.tsx
@@ -1,16 +1,18 @@
-import { JSX } from "react";
+import { JSX, ReactNode } from "react";
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import { highlight } from 'sugar-high'
 import Counter from "./counter";
 
-function Code({children, ...props}: any ) {
+type CodeProps = React.HTMLAttributes<HTMLElement> & { children: string };
+
+function Code({children, ...props}: CodeProps ) {
     const codeHTML = highlight(children)
     return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
 const components = {
     code : Code,
-    h1 : (props: any) => (
+    h1 : (props: React.HTMLAttributes<HTMLHeadingElement> & { children: ReactNode }) => (
         <h1 {...props}  className="text-3xl font-bold mt-8 mb-4"
         >{props.children}</h1>
     ),
